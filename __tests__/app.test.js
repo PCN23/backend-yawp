f39aa35177859db53f6754d491f7c2054cd54f67
Pedro <pedro.chn01@gmail.com>
2022-08-15 14:57:32 -0700

    adding first test for login user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+//const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should log in user and error if email exists', async () => {
+    const res = await request
+      .agent(app)
+      .post('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'username',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
