e2a1c43c4613566ec98bdd3abd6b8b00d24852d9
Pedro <pedro.chn01@gmail.com>
2022-08-16 15:09:28 -0700

    added test for list of restaurants endpoint

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -1,1 +1,36 @@
-describe('backend-express-template routes', () => { });
+const setup = require('../data/setup');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+
+
+describe('restaurant routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('#GET /restaurants should return a list of restaurants', async () => {
+    const resp = await request(app).get('/restaurants');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        name: 'Burger King',
+        flavor: 'burger',
+        city: 'Austin',
+        state: 'Texas',
+      },
+      {
+        id: '2',
+        name: 'King hamich',
+        flavor: 'breakfast',
+        city: 'Jonestown',
+        state: 'Florida',
+      },
+    ]);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
