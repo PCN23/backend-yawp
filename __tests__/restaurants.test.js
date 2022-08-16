2e4b04a72cf995cc67cb1470d502889eb16d591d
Pedro <pedro.chn01@gmail.com>
2022-08-16 16:02:43 -0700

    added get restaurant by id

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -11,7 +11,7 @@ describe('restaurant routes', () => {
     return setup(pool);
   });
   it('#GET /restaurants should return a list of restaurants', async () => {
-    const resp = await request(app).get('/restaurants');
+    const resp = await request(app).get('/api/v1/restaurants');
     expect(resp.status).toBe(200);
     expect(resp.body).toEqual([
       {
@@ -32,5 +32,16 @@ describe('restaurant routes', () => {
   });
   afterAll(() => {
     pool.end();
+  });
+  it('#GET restaurants/:id should return a single restaurant', async () => {
+    const resp = await request(app).get('/restaurants/2');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: '2',
+      name: 'King hamich',
+      flavor: 'breakfast',
+      city: 'Jonestown',
+      state: 'Florida',
+    });
   });
 });
