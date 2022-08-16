32ea6d65ede8b3bc4eee1958ea4edff0f1f3f8c1
Pedro <pedro.chn01@gmail.com>
2022-08-16 11:36:56 -0700

    added authoize middle ware and get all static function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,16 @@ const setup = require('../data/setup');
 //const { request } = require('express');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserServices');
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? user.password;
+  const agent = request.agent(app);
+  const newUser = await UserService.create({ ...user, ...userProps });
+  const { email } = newUser;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, newUser];
+};
 
 const user = {
   email: 'james@james.com',
@@ -28,5 +38,15 @@ describe('backend-express-template routes', () => {
   });
   afterAll(() => {
     pool.end();
+  });
+
+  it('it should sign in user and error if email and password dont match', async () => {
+    await request(app).post('/api/v1/users').send(user);
+    const res = await request(app).post('/api/v1/users/sessions').send({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(res.body).toEqual({ message: 'Signed in successfully!' });
   });
 });
